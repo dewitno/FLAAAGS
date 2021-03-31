06de1bec9a0b5ee850e225069459267038ba11c9
dewitno <norbert.dewit@student.onslow.school.nz>
2021-04-01 12:03:48 +1300

    Initial sharing of project

---
+++ FLAAGS.java
@@ -0,0 +1,31 @@
+/* Based on the ecs 100 template
+ * Code for ??
+ * Name:
+ * Date:
+ */
+
+
+import ecs100.*;
+import java.util.*;
+import java.io.*;
+import java.awt.Color;
+
+
+/** <description of class FLAAGS>
+ */
+public class FLAAGS{
+
+    /**      */
+    public FLAAGS(){
+    UI.initialise();
+    UI.addButton("Quit", UI::quit);
+    }
+
+
+
+    public static void main(String[] args){
+        FLAAGS obj = new FLAAGS();
+    }
+
+}
+
